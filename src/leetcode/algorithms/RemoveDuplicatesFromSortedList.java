d02acda8c5f2f2bfa7e97ff20697e61b5e4e7aea
Clyedhol <1269623739@qq.com>
2016-05-09 16:39:56 +0800

    UglyNumber

--- src/leetcode/algorithms/RemoveDuplicatesFromSortedList.java
+++ src/leetcode/algorithms/RemoveDuplicatesFromSortedList.java
@@ -1,5 +1,23 @@
 package leetcode.algorithms;
 
-public class RemoveDuplicatesFromSortedList {
+import java.util.HashSet;
+import java.util.Set;
 
+public class RemoveDuplicatesFromSortedList {
+	public ListNode deleteDuplicates(ListNode head) {
+		Set<Integer> s4Nodes = new HashSet<Integer>();
+		ListNode headtemp = head;
+		s4Nodes.add(headtemp.val);
+		while(headtemp != null){
+			if(headtemp.next !=null){
+				if(s4Nodes.contains(headtemp.next.val)){
+					headtemp.next = headtemp.next.next;
+				}else{
+					headtemp = headtemp.next;
+					s4Nodes.add(headtemp.val);
+				}
+			}
+		}
+        return head;
+    }
 }
---
+++ src/leetcode/algorithms/UglyNumber.java
@@ -0,0 +1,17 @@
+package leetcode.algorithms;
+
+public class UglyNumber {
+	public boolean isUgly(int num) {
+        while(num%2==0){
+        	num /= 2;
+        }
+        while(num%3==0){
+        	num /= 3;
+        }
+        while(num%5==0){
+        	num /= 5;
+        }
+        if(num == 1){return true;}
+        return false;
+    }
+}
