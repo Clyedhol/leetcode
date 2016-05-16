292758523fd876fe4256963738fd2d61da2a3440
Clyedhol <1269623739@qq.com>
2016-05-16 16:17:30 +0800

    网上学习到的很屌的方法

--- src/leetcode/algorithms/ReverseVowelsOfAString.java
+++ src/leetcode/algorithms/ReverseVowelsOfAString.java
@@ -2,17 +2,16 @@ package leetcode.algorithms;
 
 public class ReverseVowelsOfAString {
 	public String reverseVowels(String s) {
-        StringBuilder sb = new StringBuilder(s.toLowerCase());
         StringBuilder st = new StringBuilder();
         StringBuilder sg = new StringBuilder(s);
-        int n = sb.length();
-        String temp = "aeiou";
+        int n = sg.length();
         for(int i = 0;i<n;i++){
-        	if(temp.contains(sb.charAt(i)+"")){
+        	if("AEIOUaeiou".indexOf(s.charAt(i)) != -1){
         		st.append(sg.charAt(i)+"");
         		sg.replace(i, i+1, "/");
         	}
         }
+        if(st.length() == 0 || st.length() == 1){return s;}
         st.reverse();
         System.out.println(st);
         System.out.println(sg);
@@ -36,4 +35,25 @@ public class ReverseVowelsOfAString {
 		haha.replace(7, 9, "/");
 		System.out.println(haha);
 	}
+	
+	//这个方法很屌！！
+	public String reverseVowels1(String s) {
+        StringBuilder sb = new StringBuilder();
+       int j = s.length() - 1;
+       for (int i = 0; i < s.length(); i++)
+       {
+           if ("AEIOUaeiou".indexOf(s.charAt(i)) != -1)
+           {
+               while (j >= 0 && "AEIOUaeiou".indexOf(s.charAt(j)) == -1)
+               {
+                   j--;
+               }
+               sb.append(s.charAt(j));
+               j--;
+           }
+           else
+               sb.append(s.charAt(i));
+       }
+       return sb.toString();
+       }
 }
