9ce3903115f490451bf1ed7bd607de7ed81f79af
Clyedhol <1269623739@qq.com>
2016-06-13 18:14:11 +0800

    写的不好，需要改进

---
+++ src/leetcode/algorithms/TwoSum.java
@@ -0,0 +1,14 @@
+package leetcode.algorithms;
+
+import java.util.Arrays;
+
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		Arrays.sort(nums);
+		int length = nums.length;
+		int i = 0;
+		while(i<length && nums[i] < target + 1){
+			
+		}
+    }
+}
---
+++ src/leetcode/algorithms/ValidSudoku.java
@@ -0,0 +1,65 @@
+package leetcode.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidSudoku {
+	public boolean isValidSudoku(char[][] board) {
+		for(int i = 0;i<9;i++){
+			if(isValidNine(board[i]));
+			else return false;
+		}
+		char[] temp4column = new char[9];
+		for(int i = 0;i<9;i++){
+			for(int j = 0;j<9;j++)
+				temp4column[j] = board[j][i];
+			if(isValidNine(temp4column));
+			else return false;
+		}
+		for(int i = 0;i < 9;i = i+3){
+			for(int j = 0;j < 9;j = j+3){
+				getArray(temp4column,board,i,j);
+				if(isValidNine(temp4column));
+				else return false;
+			}
+		}
+        return true;
+    }
+	
+	public boolean isValidNine(char[] nine){
+		Map<String,Integer> map = new HashMap<String,Integer>();
+		for(int i = 1;i<10;i++){
+			map.put(i+"", 0);
+		}
+		map.put(".", 0);
+		for(int i = 0;i<9;i++){
+			if(!(nine[i]+"").equals(".")){
+				int temp = map.get(nine[i]+"") + 1;
+				if(temp > 1) return false;
+				map.put(nine[i]+"", temp);
+			}
+		}
+		return true;
+	}
+	
+	public void getArray(char[] temp4column,char[][] board,int i,int j){
+		int m = 3,k = 0;
+		while(m != 0){
+			int n = 3;
+			while(n != 0){
+				temp4column[k] = board[i][j];
+				i++;
+				n--;
+				k++;
+			}
+			j++;
+			m--;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ValidSudoku vs = new ValidSudoku();
+		char[] board = {'.','5','.','3','.','.','.','.','3'};
+		System.out.println(vs.isValidNine(board));
+	}
+}
