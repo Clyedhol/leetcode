24b9e3b652f7c2ce79d554d8d4de15564096c3ab
Clyedhol <1269623739@qq.com>
2016-06-16 20:04:23 +0800

    尽量用数组

---
+++ src/leetcode/algorithms/IsomorphicStrings.java
@@ -0,0 +1,42 @@
+package leetcode.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+	public boolean isIsomorphic(String s, String t) {
+        char[] sforChar = s.toCharArray();
+        char[] tforChar = t.toCharArray();
+        int[] sforA = new int[sforChar.length];
+        int[] tforA = new int[sforChar.length];
+        getIsomorphic(sforA,sforChar);
+        getIsomorphic(tforA,tforChar);
+        return isequal(sforA,tforA);
+    }
+	
+	private void getIsomorphic(int[] forA,char[] sforChar){
+		int n = 1;
+		int[] arrayforint = new int[128];
+		for(int i = 0;i<sforChar.length;i++){
+        	if(arrayforint[sforChar[i]] == 0) {
+        		n++;
+        		arrayforint[sforChar[i]] = n;
+        	}
+        	forA[i] = arrayforint[sforChar[i]];
+        }
+	}
+	
+	private boolean isequal(int[] s,int[] t){
+		for(int i = 0;i<s.length;i++){
+			if(s[i] != t[i]) return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		IsomorphicStrings is = new IsomorphicStrings();
+		System.out.println(is.isIsomorphic("11", "25"));
+		int a = 'z';
+		System.out.println(a);
+	}
+}
--- src/leetcode/algorithms/TwoSum.java
+++ src/leetcode/algorithms/TwoSum.java
@@ -1,14 +1,69 @@
 package leetcode.algorithms;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class TwoSum {
 	public int[] twoSum(int[] nums, int target) {
-		Arrays.sort(nums);
-		int length = nums.length;
-		int i = 0;
-		while(i<length && nums[i] < target + 1){
-			
+		int[] result = new int[2];
+		Set<Integer> existenceSet = new HashSet<Integer>();
+		for(int i = 0;i<nums.length-1;i++){
+			if(existenceSet.add(nums[i])){
+				for(int j = i+1;j<nums.length;j++){
+					if((nums[i] + nums[j]) == target){
+						result[0] = i;
+						result[1] = j;
+						return result;
+					}
+				}
+			}
 		}
+		return  result;
     }
+	
+	public int[] twoSum2(int[] nums, int target) {
+		int[] result = new int[2];
+		List<Mapfor> xulie = new ArrayList<Mapfor>();
+		for(int i = 0;i<nums.length;i++){
+			Mapfor mf = new Mapfor(i,nums[i]);
+			xulie.add(mf);
+		}
+		Collections.sort(xulie,new Comparator<Mapfor>() {
+			@Override
+			public int compare(Mapfor o1, Mapfor o2) {
+				if(o1.j > o2.j) return 1;
+				if(o1.j < o2.j) return -1;
+				return 0;
+			}
+		});
+		for(int i = 0;i< nums.length-1;i++){
+			for(int j = i+1;j < nums.length;j++){
+				if((xulie.get(i).j+xulie.get(j).j) == target){
+					result[0] = xulie.get(i).i;
+					result[1] = xulie.get(j).i;
+					return result;
+				}else if((xulie.get(i).j+xulie.get(j).j) > target){
+					break;
+				}
+			}
+		}
+		return result;
+    }
+	
+	class Mapfor{
+		public final int i;
+		public final int j;
+		public Mapfor(int i,int j){
+			this.i = i;
+			this.j = j;
+		}
+	}
+	
+	
 }
