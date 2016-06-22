63598cd61ecc2ca820aea23b3f47f851cf6a9310
Clyedhol <1269623739@qq.com>
2016-06-22 14:50:35 +0800

    用了之前的链表reserve的函数

---
+++ src/leetcode/algorithms/PalindromeLinkedList.java
@@ -0,0 +1,27 @@
+package leetcode.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromeLinkedList {public boolean isPalindrome(ListNode head) {
+    if(head == null || head.next == null) return true;
+    ListNode slow = head, fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    ListNode newHead = reverse(slow, null);
+    while(newHead != null){
+        if(head.val != newHead.val) return false;
+        head = head.next;
+        newHead = newHead.next;
+    }
+
+    return newHead == null;
+}
+ListNode reverse(ListNode head, ListNode prev){
+    if(head == null) return prev;
+    ListNode next = head.next;
+    head.next = prev;
+    return reverse(next, head);
+}}
